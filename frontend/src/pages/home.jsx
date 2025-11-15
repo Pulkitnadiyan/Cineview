77d2d1f7a78848649b423e24551b8ec06f64e7d3
Pulkitnadiyan <nadiyanpulkit06@gmail.com>
2025-11-15 13:08:06 +0530

    frontend issue fixed

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import Header from "./movies/Header";
-import MoviesContainerPage from "./Movies/MoviesContainerPage";
+import MoviesContainerPage from "./movies/MoviesContainerPage";
 
 const Home = () => {
   return (
