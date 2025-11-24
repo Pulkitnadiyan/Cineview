498ce3651cbff638d6678b64d80d30bec65f3b7b
Pulkitnadiyan <nadiyanpulkit06@gmail.com>
2025-11-24 17:46:46 +0530

    mobile fixed

--- frontend/src/components/SliderUtil.jsx
+++ frontend/src/components/SliderUtil.jsx
@@ -14,6 +14,20 @@ const SliderUtil = ({ data }) => {
       {
         breakpoint: 1024,
         settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+       {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+       {
+        breakpoint: 480,
+        settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
         },
