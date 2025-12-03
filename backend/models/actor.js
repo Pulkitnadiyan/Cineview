61420a2772710d3f61de09aac50a79f517427338
Pulkitnadiyan <nadiyanpulkit06@gmail.com>
2025-12-03 20:47:30 +0530

    fixes

--- backend/models/actor.js
+++ backend/models/actor.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const actorSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  photo: { type: String, required: true }, // URL to image
-  bio: { type: String, required: true },
+  photo: { type: String }, // URL to image
+  bio: { type: String },
   movies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }], // Filmography
 }, { timestamps: true });
 
