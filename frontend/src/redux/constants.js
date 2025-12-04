58a54eb403e8795893cfb02e32c72da9680ced1a
Pulkitnadiyan <nadiyanpulkit06@gmail.com>
2025-12-04 11:09:15 +0530

    issue fix

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -3,3 +3,4 @@ export const USERS_URL="/api/v1/users";
 export const GENRES_URL="/api/v1/genres";
 export const MOVIES_URL="/api/v1/movies";
 export const UPLOADS_URL="/api/v1/uploads";
+export const ACTORS_URL="/api/v1/actors";
