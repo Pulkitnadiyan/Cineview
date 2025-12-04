0531f6bb65b160558e219d35fc475b890d1a77e4
Pulkitnadiyan <nadiyanpulkit06@gmail.com>
2025-12-04 12:45:08 +0530

    backend

--- backend/controllers/moviecontroller.js
+++ backend/controllers/moviecontroller.js
@@ -40,7 +40,7 @@ const getAllmovies = asynchandler(async(req,res)=>{
 const getspecificmovie = asynchandler(async(req,res)=>{
     const {id}=req.params;
     // NOTE: Added populate('genre') for frontend display.
-    const specificmovie=await Movie.findById(id).populate('genre').populate('cast'); 
+    const specificmovie=await Movie.findById(id).populate('genre').populate('cast', 'name photo bio'); 
     if(!specificmovie){
         res.status(404); // Using asynchandler, we set status and throw an Error
         throw new Error("Movie not found"); 
